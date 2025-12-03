4ca42d668d07a44f3d8926196cff17dfc575e9af
livymeii <adindaameiliaa09@gmail.com>
2025-12-03 08:18:42 +0700

    p

--- model/siswa.js
+++ model/siswa.js
@@ -1,5 +1,5 @@
 const mongoose = require ('mongoose');
-const Siswa = mongoose.model('Siswa', {
+const Siswa = mongoose.model('siswa', {
         nama: {
         type: String,
         required: true,
