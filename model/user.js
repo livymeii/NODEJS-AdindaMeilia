d87a4c218dcbd90fe2f398b0b4dd32cd7aa44cfc
livymeii <adindaameiliaa09@gmail.com>
2025-12-03 08:22:43 +0700

    p

--- model/user.js
+++ model/user.js
@@ -5,4 +5,20 @@ const User = mongoose.model('user', {
   password: String,
 });
 
+async function createDefaultAdmin() {
+    const admin = await User.findOne({ username: 'admin' });
+
+    if (!admin) {
+        await User.create({
+            username: 'admin',
+            password: 'admin',
+        });
+        console.log('Admin default dibuat');
+    } else {
+        console.log('Admin sudah ada');
+    }
+}
+ 
+createDefaultAdmin();
+
 module.exports = User;
